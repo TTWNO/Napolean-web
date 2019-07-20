754ccb891e02faccf2b8740494f45326efad93dc
Tait Hoyem <44244401+TTWNO@users.noreply.github.com>
2019-07-20 12:51:11 +0000

    Add basic (not fully working) ajaj calls

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const body_parser = require("body-parser");
 const path = require('path');
 
 const publicPath = path.join(__dirname, '../public');
@@ -6,9 +7,13 @@ const publicPath = path.join(__dirname, '../public');
 let app = express();
 
 app.use(express.static(publicPath));
+app.use(body_parser.json());
 
-app.get('/', (req, res) => {
-	console.log("[REQUEST]: " + req);
+app.post('/', (req, res) => {
+	console.log("[REQUEST]: " + req.body.messege.toString());
+	res.writeHead(200, {"Content-type": "applicaiton/json"});
+	res.write({"response": "Hello world!"}.toString());
+	res.send();
 });
 
 app.listen(3000, '127.0.0.1');
