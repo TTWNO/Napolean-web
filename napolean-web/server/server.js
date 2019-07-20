3082f92495311b059c2e16b0c4544b12bdd08969
Tait Hoyem <44244401+TTWNO@users.noreply.github.com>
2019-07-20 10:45:50 +0000

    Add public dir

--- napolean-web/server/server.js
+++ napolean-web/server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, './public');
 
 let app = express();
 
+app.use(express.static(publicPath));
+
 app.listen(3000, '127.0.0.1');
 
-console.log("Running!");
+console.log("Running! on localhost:3000");
